0d7112bd4a3da8e6de1622de20441de96045def1
luffyui <tanaka.yuki000@hotmail.com>
2023-12-07 20:15:56 -0500

    docs: add fancy text to home page

--- www/components/pages/Home.tsx
+++ www/components/pages/Home.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { clamp, motion } from "framer-motion";
+import { clamp, motion, useMotionValueEvent, useScroll } from "framer-motion";
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 import { useWindowSize } from "../../hooks/useWindowSize";
 import Features from "../features";
 import Sticky from "../sticky";
@@ -18,89 +18,137 @@ const fadeIn = {
   },
 };
 
+function Gradient() {
+  const { scrollYProgress } = useScroll();
+
+  const [progress, setProgress] = useState(0);
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    setProgress(latest);
+  });
+
+  const style = {
+    opacity: clamp(0, 1, progress * 4),
+  };
+
+  return (
+    <div className="background" style={style}>
+      <div className="gradient" />
+    </div>
+  );
+}
+
 function Hero() {
   const { height } = useWindowSize();
 
   if (!height) return null;
   return (
-    <Sticky
-      cover
-      height={height}
-      render={({ progress }) => {
-        const blur = {
-          filter: `blur(${progress * 5}px)`,
-          opacity: 1 - progress,
-        };
+    <section>
+      <Sticky
+        cover
+        height={height}
+        render={({ progress }) => {
+          const blur = {
+            filter: `blur(${progress * 3}px)`,
+            opacity: 1 - progress * 1.4,
+          };
 
-        const scale = {
-          transform: `scale(${clamp(0.9, 1.1, 1.1 - progress * 0.3)})`,
-        };
+          const scale = {
+            transform: `scale(${clamp(0.9, 1.1, 1.1 - progress * 0.3)})`,
+          };
 
-        return (
-          <div className="flex flex-col justify-between text-center">
-            <div style={blur}>
-              <motion.h1
-                initial={{
-                  opacity: 0,
-                  scale: 1.2,
-                }}
-                animate={{
-                  opacity: 1,
-                  scale: 1,
-                }}
-                transition={{
-                  duration: 2.5,
-                  ease: "easeInOut",
-                }}
-                className="max-w-5xl mx-auto nx-text-6xl font-extrabold tracking-tighter leading-[1.1] text-6xl sm:text-7xl lg:nx-text-8xl xl:nx-text-8xl"
+          return (
+            <div className="flex flex-col justify-between text-center">
+              <div style={blur}>
+                <motion.h1
+                  initial={{
+                    filter: "blur(3px)",
+                    opacity: 0,
+                    scale: 1.2,
+                  }}
+                  animate={{
+                    filter: "blur(0px)",
+                    opacity: 1,
+                    scale: 1,
+                  }}
+                  transition={{
+                    duration: 2.5,
+                    ease: "easeInOut",
+                  }}
+                  className="max-w-5xl mx-auto nx-text-6xl font-extrabold tracking-tighter leading-[1.1] text-6xl sm:text-7xl lg:nx-text-8xl xl:nx-text-8xl"
+                >
+                  ERP for{" "}
+                  <span className="bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 dark:from-purple-100 dark:via-purple-200 dark:to-orange-200 bg-clip-text text-transparent">
+                    the builders
+                  </span>
+                </motion.h1>
+                <motion.p
+                  {...fadeIn}
+                  transition={{
+                    duration: 0.5,
+                    delay: 2.6,
+                    ease: "easeInOut",
+                  }}
+                  className="mt-6 nx-text-xl font-medium leading-tight text-black/60 dark:text-white/60 sm:nx-text-2xl md:nx-text-3xl lg:nx-text-4xl"
+                >
+                  Carbon is an open-source starting point for business
+                </motion.p>
+              </div>
+              <div
+                className="flex fix-flex-col h-32 mt-4 md:mt-0 mb-6 md:mb-0 items-center justify-center gap-3 md:flex-row xl:flex-row"
+                style={scale}
               >
-                ERP for the builders
-              </motion.h1>
-              <motion.p
-                {...fadeIn}
-                transition={{
-                  duration: 0.5,
-                  delay: 2.6,
-                  ease: "easeInOut",
-                }}
-                className="mt-6 nx-text-xl font-medium leading-tight text-black/60 dark:text-white/60 sm:nx-text-2xl md:nx-text-3xl lg:nx-text-4xl"
-              >
-                Carbon ERP is an open-source ERP starting point
-              </motion.p>
+                <motion.a
+                  {...fadeIn}
+                  transition={{
+                    duration: 0.5,
+                    delay: 3.0,
+                    ease: "easeInOut",
+                  }}
+                  className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-black no-underline bg-zinc-200 border border-transparent rounded-md dark:bg-zinc-800 dark:text-white betterhover:dark:hover:bg-gray-800 betterhover:hover:bg-zinc-300 md:py-3 md:text-lg md:px-10 md:leading-6 fix-width-auto xl:w-auto"
+                  href="/learn/introduction"
+                >
+                  Learn
+                </motion.a>
+                <motion.a
+                  {...fadeIn}
+                  transition={{
+                    duration: 0.5,
+                    delay: 3.2,
+                    ease: "easeInOut",
+                  }}
+                  className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white no-underline bg-zinc-800 border border-transparent rounded-md dark:bg-zinc-100 dark:text-black betterhover:dark:hover:bg-gray-300 betterhover:hover:bg-gray-700 md:py-3 md:text-lg md:px-10 md:leading-6 fix-width-auto xl:w-auto"
+                  href="/develop/getting-started"
+                >
+                  Develop
+                </motion.a>
+              </div>
             </div>
-            <div
-              className="flex fix-flex-col h-32 mt-4 md:mt-0 mb-6 md:mb-0 items-center justify-center gap-3 md:flex-row xl:flex-row"
-              style={scale}
-            >
-              <motion.a
-                {...fadeIn}
-                transition={{
-                  duration: 0.5,
-                  delay: 3.0,
-                  ease: "easeInOut",
-                }}
-                className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-black no-underline bg-zinc-200 border border-transparent rounded-md dark:bg-zinc-800 dark:text-white betterhover:dark:hover:bg-gray-800 betterhover:hover:bg-zinc-300 md:py-3 md:text-lg md:px-10 md:leading-6 fix-width-auto xl:w-auto"
-                href="/learn/introduction"
-              >
-                Learn
-              </motion.a>
-              <motion.a
-                {...fadeIn}
-                transition={{
-                  duration: 0.5,
-                  delay: 3.2,
-                  ease: "easeInOut",
-                }}
-                className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white no-underline bg-zinc-800 border border-transparent rounded-md dark:bg-zinc-100 dark:text-black betterhover:dark:hover:bg-gray-300 betterhover:hover:bg-gray-700 md:py-3 md:text-lg md:px-10 md:leading-6 fix-width-auto xl:w-auto"
-                href="/develop/getting-started"
-              >
-                Develop
-              </motion.a>
-            </div>
-          </div>
-        );
-      }}
-    />
+          );
+        }}
+      />
+    </section>
+  );
+}
+
+function Overview() {
+  return (
+    <motion.section id="overview" className="relative" {...fadeIn}>
+      <div className="px-4 py-16 mx-auto sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24">
+        <h2 className="nx-text-4xl font-extrabold tracking-tight lg:nx-text-5xl xl:nx-text-6xl lg:text-center dark:text-white">
+          The{" "}
+          <span className="bg-gradient-to-r from-purple-600 to-pink-600 dark:from-purple-100 dark:via-purple-200 dark:to-orange-200 bg-clip-text text-transparent">
+            open core
+          </span>{" "}
+          for your digital operations
+        </h2>
+        <p className="mx-auto mt-4 text-lg font-medium text-black/60 dark:text-white/60 lg:max-w-3xl lg:text-xl lg:text-center">
+          Every business is unique. Carbon gives you the basic building blocks
+          to build your own proprietary business systems with the best in
+          modern, open-source software.
+        </p>
+        <Features />
+      </div>
+    </motion.section>
   );
 }
 
@@ -115,45 +163,11 @@ export default function Home() {
         />
       </Head>
 
-      <div>
-        {/* <Gradient /> */}
+      <section>
+        <Gradient />
         <Hero />
-
-        <motion.div
-          initial={{
-            opacity: 0,
-          }}
-          animate={{
-            opacity: 1,
-          }}
-          transition={{
-            duration: 0.5,
-            ease: "easeInOut",
-            delay: 2.5,
-          }}
-          className="relative"
-        >
-          <div className="px-4 py-16 mx-auto sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24">
-            <h2 className="nx-text-4xl font-extrabold tracking-tight lg:nx-text-5xl xl:nx-text-6xl lg:text-center dark:text-white">
-              Fully customizable and extensible
-            </h2>
-            <p className="mx-auto mt-4 text-lg font-medium text-black/60 dark:text-white/60 lg:max-w-3xl lg:text-xl lg:text-center">
-              We've taken the best of modern, open-source technology and
-              combined it into a lightning-fast, easy-to-use, open-source ERP
-              system.
-            </p>
-            <Features />
-          </div>
-        </motion.div>
-      </div>
+        <Overview />
+      </section>
     </>
   );
 }
-
-// function Gradient() {
-//   return (
-//     <div className="background">
-//       <div className="gradient" />
-//     </div>
-//   );
-// }
