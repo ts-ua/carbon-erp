1247f88acd6e6c12476b0d1fb116938d3eb3c0da
luffyui <tanaka.yuki000@hotmail.com>
2024-02-13 21:49:58 -0600

    refactor: hide incomplete rfq submodule

--- apps/carbon/app/modules/purchasing/ui/RequestForQuote/RequestForQuoteForm.tsx
+++ apps/carbon/app/modules/purchasing/ui/RequestForQuote/RequestForQuoteForm.tsx
@@ -72,7 +72,7 @@ const RequestForQuoteForm = ({ initialValues }: RequestForQuoteFormProps) => {
               )}
             >
               <VStack>
-                <Input name="name" label="Name" />
+                <Input autoFocus name="name" label="Name" />
                 <DatePicker name="receiptDate" label="Receipt Date" />
                 <DatePicker name="expirationDate" label="Expiration Date" />
               </VStack>
